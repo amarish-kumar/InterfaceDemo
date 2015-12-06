3ef8059558a068eebe0124aecf1798fc9ad1f437
rahul sahay <rahulsahay19@gmail.com>
2015-12-06 20:28:49 +0530

    2nd commit

--- InterfaceDemo/Program.cs
+++ InterfaceDemo/Program.cs
@@ -9,10 +9,25 @@ namespace InterfaceDemo
             // ITransaction transaction = new Transaction();
             // transaction.doTransaction();
             //Console.WriteLine(transaction.getAmount);
-            Transaction transaction = new Transaction();
+            //Transaction transaction = new Transaction();
+            //transaction.startTransaction();
+            //transaction.doTransaction();
+            //Console.WriteLine(transaction.getAmount);
+            var transaction = new Transaction();
             transaction.startTransaction();
             transaction.doTransaction();
             Console.WriteLine(transaction.getAmount);
+
+            //Assigned the object to Interface variable
+            ITransaction iTransaction = transaction;
+            //Here also only interface stuffs will be available
+            //However, if at all we would like to transaction explicit method available, then we need to cast like shown below
+            Transaction transactionObj = iTransaction as Transaction;
+            //Here, as keyword will examine the iTransaction and if it is of type Transaction, then it will return Transaction
+            //else will return null. Hence, null check is recommended here
+            if(transactionObj!=null)
+                transactionObj.startTransaction();
+
             Console.ReadLine();
 
         }
